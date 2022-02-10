94b3bb62e903b5388e41702b5e3dabc74854e675
bybang <bybang26@gmail.com>
2022-02-09 19:12:19 -0500

    Refactor getAllProperties function to use search function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -120,9 +120,67 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // if city passed in, returns city name including keyword
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  // if owner_id passed in, returns properties
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}`);
+    if (queryParams.length > 1) {
+      queryString += `AND owner_id LIKE $${queryParams.length}`;
+    }
+    queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+  }
+
+  // if min and max price passed in, return properties within
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    if (queryParams.length > 2) {
+      queryString += `AND cost_per_night >= $${
+        queryParams.length - 1
+      } AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `WHERE cost_per_night >= $${
+        queryParams.length - 1
+      } AND cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
+  // if min rating passed in, return properties with rating above it
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    if (queryParams.length > 1) {
+      queryString += `AND `;
+    } else {
+      queryString += `WHERE `;
+    }
+    queryString += `rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
@@ -134,10 +192,49 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+  // return pool
+  //   .query(
+  //     ` INSERT INTO properties(
+  //     owner_id,
+  //     title,
+  //     description,
+  //     thumbnail_photo_url,
+  //     cover_photo_url,
+  //     cost_per_night,
+  //     street,
+  //     city,
+  //     province,
+  //     post_code,
+  //     country,
+  //     parking_spaces,
+  //     number_of_bathrooms,
+  //     number_of_bedrooms)
+  //     VALUES(
+  //       $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 $13, $14) RETURNING *;`,
+  //     [
+  //       property.owner_id,
+  //       property.title,
+  //       property.description,
+  //       property.thumbnail_photo_url,
+  //       property.cover_photo_url,
+  //       property.cost_per_night,
+  //       property.street,
+  //       property.city,
+  //       property.province,
+  //       property.post_code,
+  //       property.country,
+  //       property.parking_spaces,
+  //       property.number_of_bathrooms,
+  //       property.number_of_bedrooms,
+  //     ]
+  //   )
+  //   .then((result) => {
+  //     return result.rows[0];
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //     return null;
+  //   });
 };
 exports.addProperty = addProperty;
