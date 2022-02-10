b5667fe76f56cbb3ec8e18fb6c2de04eb99468b9
bybang <bybang26@gmail.com>
2022-02-09 20:54:52 -0500

    Update AddProperty function to creat new list(place), fix error with getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -136,11 +136,12 @@ const getAllProperties = (options, limit = 10) => {
 
   // if owner_id passed in, returns properties
   if (options.owner_id) {
-    queryParams.push(`%${options.owner_id}`);
+    const owner_id = parseInt(options.owner_id);
+    queryParams.push(owner_id);
     if (queryParams.length > 1) {
-      queryString += `AND owner_id LIKE $${queryParams.length}`;
+      queryString += `AND owner_id = $${queryParams.length}`;
     }
-    queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+    queryString += `WHERE owner_id = $${queryParams.length}`;
   }
 
   // if min and max price passed in, return properties within
@@ -193,48 +194,48 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = (property) => {
-  // return pool
-  //   .query(
-  //     ` INSERT INTO properties(
-  //     owner_id,
-  //     title,
-  //     description,
-  //     thumbnail_photo_url,
-  //     cover_photo_url,
-  //     cost_per_night,
-  //     street,
-  //     city,
-  //     province,
-  //     post_code,
-  //     country,
-  //     parking_spaces,
-  //     number_of_bathrooms,
-  //     number_of_bedrooms)
-  //     VALUES(
-  //       $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 $13, $14) RETURNING *;`,
-  //     [
-  //       property.owner_id,
-  //       property.title,
-  //       property.description,
-  //       property.thumbnail_photo_url,
-  //       property.cover_photo_url,
-  //       property.cost_per_night,
-  //       property.street,
-  //       property.city,
-  //       property.province,
-  //       property.post_code,
-  //       property.country,
-  //       property.parking_spaces,
-  //       property.number_of_bathrooms,
-  //       property.number_of_bedrooms,
-  //     ]
-  //   )
-  //   .then((result) => {
-  //     return result.rows[0];
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     return null;
-  //   });
+  return pool
+    .query(
+      `
+      INSERT INTO properties(
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        street,
+        city,
+        province,
+        post_code,
+        country,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+      ]
+    )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 };
 exports.addProperty = addProperty;
